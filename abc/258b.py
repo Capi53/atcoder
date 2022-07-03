a551654cb1d79176ebe115ea3a2e75584d1090b8
かぴ <44591777+Capi53@users.noreply.github.com>
2022-07-03 19:54:03 +0900

    Create 258b.py

---
+++ abc/258b.py
@@ -0,0 +1,22 @@
+n = int(input())
+field = [list(map(int, list(input()))) for _ in range(n)]
+ans = 0
+ope = []
+for i in range(-1, 2):
+  for j in range(-1, 2):
+    ope.append([i,j])
+
+for i in range(n):
+  for j in range(n):
+    x = j
+    y = i
+    for dx, dy in ope:
+      tmp = 0
+      for _ in range(n):
+        tmp = tmp*10 + field[y][x]
+        y += dy
+        x += dx
+        y %= n
+        x %= n
+      ans = max(ans, tmp)
+print(ans)
