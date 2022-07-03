2cc74b734f5afae66fd9647962f4f30d98a7e490
かぴ <44591777+Capi53@users.noreply.github.com>
2022-07-03 20:09:08 +0900

    Update 258c.py

--- abc/258c.py
+++ abc/258c.py
@@ -1,9 +1,13 @@
 N, Q = map(int, input().split())
 s = input()
-query = [list(map(int, input().split())) for _ in range(Q)] # 1 x , 2 x
+query = [list(map(int, input().split())) for _ in range(Q)]
 
-for q, x in query:
-  if q == 2:
-    print(s[x-1])
+p = 0
+for i in range(Q):
+  q = query[i][0]
+  x = query[i][1]
+
+  if q == 1:
+    p -= x
   else:
-    s = s[-x:][::-1] + s[:-x]
+    print(s[(x - 1 + p) % N])
